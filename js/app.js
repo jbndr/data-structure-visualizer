bd485c8bcdd29cb73a149d2b334de48ca31a30be
Johannes Binder <binderjohannes@outlook.com>
2020-03-11 12:57:44 +0100

    Refactor code and create rotating cube

--- js/app.js
+++ js/app.js
@@ -1,27 +1,68 @@
-const container = document.querySelector('#scene-container');
+let container;
+let camera;
+let renderer;
+let scene;
+let mesh;
 
-const scene = new THREE.Scene();
+function init() {
 
-scene.background = new THREE.Color('blue');
+    container = document.querySelector('#scene-container');
 
-const fov = 35; 
-const aspect = container.clientWidth / container.clientHeight;
-const near = 0.1; 
-const far = 100;
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color('gray');
 
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.set(0, 0, 10);
+    createCamera();
+    createMeshes();
+    createRenderer();
 
-const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
-const material = new THREE.MeshBasicMaterial();
-const mesh = new THREE.Mesh( geometry, material );
+}
 
-scene.add(mesh);
+function createCamera() {
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(container.clientWidth, container.clientHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
+    const fov = 35; 
+    const aspect = container.clientWidth / container.clientHeight;
+    const near = 0.1; 
+    const far = 100;
+    
+    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 0, 10);
 
-container.appendChild(renderer.domElement);
-renderer.render(scene, camera);
+}
 
+function createMeshes() {
+
+    const geometry = new THREE.BoxBufferGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0xEEEEEE });
+    mesh = new THREE.Mesh( geometry, material );
+
+    scene.add(mesh);
+
+}
+
+function createRenderer() {
+
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    renderer.gammaFactor = 2.2;
+    renderer.gammaOutput = true;
+
+    container.appendChild(renderer.domElement);
+
+}
+
+function animate(){
+
+    window.requestAnimationFrame(animate);
+
+    mesh.rotation.z += 0.01;
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+
+}
+
+init();
+animate();
